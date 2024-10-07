aefa4409ad870a6ec3321104bbb335c85e5d7547
David <davidopotowsky14@gmail.com>
2024-10-07 12:37:21 -0500

    get apps

--- server.js
+++ server.js
@@ -1,1 +1,23 @@
 // TODO: this file :)
+
+const express = require("express");
+const app = express ();
+const PORT = 3000;
+const employees = reuire("./employees");
+
+app.get("/", (req, res) => {
+    res.send("Hello employees!");
+});
+
+app.get("/employees", (req, res) => {
+    res.json(employees);
+});
+
+
+
+
+
+
+app.listen(PORT, () => {
+    console.log(`Listening on port ${PORT}`)
+})
