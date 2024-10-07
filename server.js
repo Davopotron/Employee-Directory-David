08e5f05d07045bd234cb3e2dc8c025cc2e60ae6d
David <davidopotowsky14@gmail.com>
2024-10-07 13:38:16 -0500

    typed up get apps and random employee function

--- server.js
+++ server.js
@@ -1,23 +1,39 @@
 // TODO: this file :)
 
 const express = require("express");
-const app = express ();
+const app = express();
 const PORT = 3000;
-const employees = reuire("./employees");
+const employees = require("./employees");
 
 app.get("/", (req, res) => {
     res.send("Hello employees!");
 });
 
+
 app.get("/employees", (req, res) => {
     res.json(employees);
+});
+
+app.get("/employees/random", (req, res) =>{
+    const randomIndex = Math.floor(Math.random() * employees.length);
+    const randomEmployee = employees[randomIndex];
+
+    res.send(randomEmployee.name); 
+});
+
+app.get("/employees/:id", (req, res) => {
+    const employee = employees.find((obj) =>
+    obj.id === +req.params.id);
+    if (employee) {
+        res.send(employee.name);
+    } else {
+        res.send("id not located")
+    }
 });
 
 
 
 
-
-
 app.listen(PORT, () => {
-    console.log(`Listening on port ${PORT}`)
-})
+    console.log(`Listening on port ${PORT}`);
+});
